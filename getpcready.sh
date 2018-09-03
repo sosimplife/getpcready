e55606b45ea3863c328f8e2d8aab7c6c96f50f1a
sosimplife <yansumail@foxmail.com>
2018-09-04 00:16:55 +0800

    Create getpcready.sh
    Add variant "PATH".

---
+++ getpcready.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This script is used to set up the Debian or other based operation system on your pc very quickly.
+# History:
+# 2018/09/03    tim    First edition
+
+PATH = /bin:/sbin:/usr/bin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+
+exit 0
